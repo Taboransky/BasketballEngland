6615a5700ba886740135f94933a9677c013a0265
Taboransky <a.taboransky@gmail.com>
2025-01-10 22:46:52 +0100

    I lied :) last commit with Geminis help?

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/resources/features"},
         glue = "stepDefinitions",
-        plugin = {"pretty", "json:target/cucumber-report.json"}
+        plugin = {"pretty", "html:target/cucumber-reports"}
 )
 
 public class TestRunner {}
